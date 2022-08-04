6f282b85dfead2e0a7d1d48a47175b17c6098d00
ekwaro <dominicnoelchris@gmail.com>
2022-08-04 09:11:16 +0300

    configuring application to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import Navbar from './components/navbar';
 import './App.css';
+import {Hero,Analytics, Newsletter, Cards, Footer} from './components/Hero';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-4 p-3">
+      <div className='mt-4'>
+      <Navbar />
+      </div>
+      
+      <div className='flex-col mx-5 flex '>
+        
+        <Hero/>
+        <div className='mt-[-120px]'>
+          <Newsletter />
+        </div>
+      </div>
+     
+    
+      <Footer/>
     </div>
   );
 }
